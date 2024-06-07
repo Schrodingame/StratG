d2cd30aa93d6acf7af61e42b093400f9401cd85d
Veadazak <Alex91kaz@gmail.com>
2024-06-07 15:05:09 +0300

    New main scene

--- Test.gd
+++ Test.gd
@@ -5,7 +5,7 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
-# Helo worlaaaaaa2
+# Helo worlaaaaaa
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass

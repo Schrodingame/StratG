6da07b5b5f48c9c681eea41677a951c23f520a3c
Veadazak <Alex91kaz@gmail.com>
2024-06-07 21:23:12 +0300

    NotImportantAtAll

--- Scripts/UI/node_2d.gd
+++ Scripts/UI/node_2d.gd
@@ -20,9 +20,10 @@ func inc():
 
 
 func _on_button_pressed():
+	OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 	inc()
 	print(test_inc)
 	$Label.text = "meow"
 	$Button.text = "Никита"
-	$Button.icon = preload("res://milye-zveriushki.-4-1.webp")
+	$Button.icon = preload("res://Media/milye-zveriushki.-4-1.webp")
 	pass # Replace with function body.

a7702caede6f50800fdba61860ba3ce4952d6f7d
Veadazak <Alex91kaz@gmail.com>
2024-06-10 00:12:00 +0300

    Визуализация костей

--- Scripts/Field/GameControll.gd
+++ Scripts/Field/GameControll.gd
@@ -10,8 +10,20 @@ func _ready():
 			cellsArray.append(child)
 	##cellsArray.sort()
 	print(cellsArray)
+	#Вариант автоматической расстановки маркеров
+	"""for i in range(39):
+		if i==0:
+			cellsArray[i].global_position = Vector2i(60,60)
+		elif i==10:
+			cellsArray[i].global_position = Vector2i(60,60)
+		elif i==20:
+			cellsArray[i].global_position = Vector2i(60,60)
+		elif i==30:
+			cellsArray[i].global_position = Vector2i(60,60)
+		
+		cellsArray[i].global_position = Vector2i(0,0)"""
 	actCell=0
-	$PlayerBody.moveToCell(cellsArray[0].global_position)
+	$"Player#1".moveToCell(cellsArray[0].global_position)
 	$Money.text=str(money)
 func _on_button_pressed(): #Наработки Никиты Г. в мультимедиа.
 	OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")	
@@ -31,9 +43,59 @@ func _on_dice_drop_pressed():
 	if (actCell==5||actCell==15||actCell==25||actCell==35) :
 		OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 	$Dice_1.text=str(dice_1)
+	if dice_1==1: 
+		$Dice_1_visual/Side1Pip.set_visible(true)
+	else:
+		$Dice_1_visual/Side1Pip.set_visible(false)
+	if dice_1==2: 
+		$Dice_1_visual/Side2Pips.set_visible(true)
+	else:
+		$Dice_1_visual/Side2Pips.set_visible(false)
+	if dice_1==3: 
+		$Dice_1_visual/Side3Pips.set_visible(true)
+	else:
+		$Dice_1_visual/Side3Pips.set_visible(false)
+	if dice_1==4: 
+		$Dice_1_visual/Side4Pips.set_visible(true)
+	else:
+		$Dice_1_visual/Side4Pips.set_visible(false)
+	if dice_1==5: 
+		$Dice_1_visual/Side5Pips.set_visible(true)
+	else:
+		$Dice_1_visual/Side5Pips.set_visible(false)
+	if dice_1==6: 
+		$Dice_1_visual/Side6Pips.set_visible(true)
+	else:
+		$Dice_1_visual/Side6Pips.set_visible(false)
+	
+	if dice_2==1: 
+		$Dice_2_visual/Side1Pip.set_visible(true)
+	else:
+		$Dice_2_visual/Side1Pip.set_visible(false)
+	if dice_2==2: 
+		$Dice_2_visual/Side2Pips.set_visible(true)
+	else:
+		$Dice_2_visual/Side2Pips.set_visible(false)
+	if dice_2==3: 
+		$Dice_2_visual/Side3Pips.set_visible(true)
+	else:
+		$Dice_2_visual/Side3Pips.set_visible(false)
+	if dice_2==4: 
+		$Dice_2_visual/Side4Pips.set_visible(true)
+	else:
+		$Dice_2_visual/Side4Pips.set_visible(false)
+	if dice_2==5: 
+		$Dice_2_visual/Side5Pips.set_visible(true)
+	else:
+		$Dice_2_visual/Side5Pips.set_visible(false)
+	if dice_2==6: 
+		$Dice_2_visual/Side6Pips.set_visible(true)
+	else:
+		$Dice_2_visual/Side6Pips.set_visible(false)
+	
 	$Dice_2.text=str(dice_2)
 	print ("Result = ",result)
 	print("ActCell = ",actCell)
-	$PlayerBody.moveToCell(cellsArray[actCell].global_position)	
+	$"Player#1".moveToCell(cellsArray[actCell].global_position)	
 	$Money.text=str(money)
 	
--- Scripts/Player/PlayerControl.gd
+++ Scripts/Player/PlayerControl.gd
@@ -15,8 +15,6 @@ func moveToCell(newPos:Vector2):
 	move_and_slide()	"""
 	
 func _physics_process(delta):
-	"""var direction = Input.get_vector("left","right","up","down")
-	velocity = direction * 600"""
 	var direction = global_position.direction_to(target_position)
 	if position.distance_to(target_position)>3:
 		velocity = direction*300.0

06516cfe0c2a8e7957ea3f10e0399a40653f95b3
Veadazak <Alex91kaz@gmail.com>
2024-06-11 12:22:42 +0300

    изменения в контроле

--- Scripts/Field/GameControll.gd
+++ Scripts/Field/GameControll.gd
@@ -4,47 +4,34 @@ var cellsArray = []
 var result
 var actCell
 var money=15000
+var players=[]
+var actPlayer:int=0
 
 func _ready():
 	for child in $Markers.get_children():
 		if child is Marker2D:
-			cellsArray.append(child)
-	##cellsArray.sort()
+			cellsArray.append(child)	
 	print(cellsArray)
-	
+	for child in $".".get_children():
+		if child is CharacterBody2D:
+			players.append(child)
+	print(players)
 	$Dice_1_visual.update_value( 1 );
 	$Dice_2_visual.update_value( 6 );
-	#Вариант автоматической расстановки маркеров
-	"""for i in range(39):
-		if i==0:
-			cellsArray[i].global_position = Vector2i(60,60)
-		elif i==10:
-			cellsArray[i].global_position = Vector2i(60,60)
-		elif i==20:
-			cellsArray[i].global_position = Vector2i(60,60)
-		elif i==30:
-			cellsArray[i].global_position = Vector2i(60,60)
-		
-		cellsArray[i].global_position = Vector2i(0,0)"""
 	actCell=0
 	$"Player#1".moveToCell(cellsArray[0].global_position)
 	$Money.text=str(money)
-
-func _on_button_pressed(): #Наработки Никиты Г. в мультимедиа.
-	OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")	
-	#$Label.text = "meow"
-	$Button.text = "Никита"
-	$Button.icon = preload("res://Media/milye-zveriushki.-4-1.webp")
-	pass 
+	#add_child( MonopolyPlayer.new() ) На будующее для добавления игроков по подключениям
+	
 
 func _on_dice_drop_pressed():
 	var dice_1 = $Dice_1_visual.roll();
-	var dice_2 = $Dice_2_visual.roll();
+	var dice_2 = $Dice_2_visual.roll();	
 	result = dice_1+dice_2
-	if (actCell+result)>=39:
+	"if (actCell+result)>=39:
 		actCell=(actCell+result)-40
 		money+=2000
-	else : actCell+=result
+	else : actCell+=result"
 
 	if (actCell==5||actCell==15||actCell==25||actCell==35) :
 		# Sorry, it bothers me...
@@ -53,6 +40,11 @@ func _on_dice_drop_pressed():
 
 	print ("Result = ",result)
 	print("ActCell = ",actCell)
-	$"Player#1".moveToCell(cellsArray[actCell].global_position)	
+	players[actPlayer].yourTern(result)
+	#$"Player#1".moveToCell(cellsArray[actCell].global_position)	
 	$Money.text=str(money)
-	
+	nextPlayerMove()
+func nextPlayerMove():
+	if actPlayer+1<players.size():
+		actPlayer+=1
+	else: actPlayer=0
--- Scripts/Player/PlayerControl.gd
+++ Scripts/Player/PlayerControl.gd
@@ -1,24 +1,28 @@
-
+class_name MonopolyPlayer
 extends CharacterBody2D
-
-
 var target_position = Vector2()
-
-
+#var result:int
+var actCell:int=0
+var money=15000
 
 func moveToCell(newPos:Vector2):	
 	target_position=newPos
-	Global.send_message( "moved to pos " + str( newPos ), 1 )
-	"""var direction = global_position.direction_to(target_position)
-	if position.distance_to(target_position)>3:
-		velocity = direction*300.0
-	else: velocity=direction*0
-	move_and_slide()	"""
+	Global.send_message( "moved to pos " + str( newPos ), 1 )	
 	
 func _physics_process(_delta):
 	var direction = global_position.direction_to(target_position)
 	if position.distance_to(target_position)>3:
 		velocity = direction*300.0
 	else: velocity=direction*0
+		#get_Parent().actPlayer
+		 # .nextPlayerMove()
 	
 	move_and_slide()
+	
+func yourTern(result:int):
+	if (actCell+result)>=39:
+		actCell=(actCell+result)-40
+		money+=2000
+	else : actCell+=result
+	moveToCell($"..".cellsArray[actCell].global_position)
+	#moveToCell()
